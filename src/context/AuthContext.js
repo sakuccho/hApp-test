8b63690f1410ffa7ce3ad29109b3a78420082e4e
sakuccho <karuteeto@yahoo.co.jp>
2022-03-14 12:30:50 +0900

    loding

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -27,7 +27,7 @@ export function AuthProvider({ children }) {
   }, []);
 
   if (loading) {
-    return <p>loading...</p>;
+    return <p>loding...</p>;
   } else {
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
   }
